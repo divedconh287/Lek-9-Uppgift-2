17d53754e5b196c05245e6e04b57f7935000f0fb
divedconh287 <divedconh287@gmail.com>
2025-11-10 19:46:21 +0100

    så mycket att göra

---
+++ script.js
@@ -0,0 +1,28 @@
+const tal1input = document.querySelector("tal1");
+const tal2input = document.querySelector("tal2");
+// const?
+
+function plus(){
+const tal1= parseInt(tal1input.value);
+const tal2= parseInt(tal2input.value);
+}
+
+function minus(){
+
+}
+
+function ggr(){
+
+}
+
+function dela(){
+
+}
+
+let output = document.querySelector("output");
+output.appendChild(p);
+tal1input.value = "";
+tal2input.value = "";
+// output grejer
+
+console.log("Matte!");
