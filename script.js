980d303d31a131ff0e9aed93bb2802e7f6f07378
Aaron Granath TE24C <divedconh287@gmail.com>
2025-11-11 14:30:52 +0100

    klar! med Knattehjälp

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-const tal1input = document.querySelector("tal1");
-const tal2input = document.querySelector("tal2");
-// const?
+const tal1input = document.querySelector("#tal1");
+const tal2input = document.querySelector("#tal2");
+let svar = document.querySelector("#svar");
+let output = document.querySelector("#output");
 
+/* + - * / modulo % */ 
 function plus(){
-const tal1= parseInt(tal1input.value);
-const tal2= parseInt(tal2input.value);
+    const tal1= parseFloat(tal1input.value);
+    const tal2= parseFloat(tal2input.value);
+    let summa = tal1+tal2;
+    svar.value=summa;
+    let p = document.createElement("p");
+    p.textContent= tal1+"+"+tal2+"="+summa;
+    output.appendChild(p);
+    tal1input.value = "";
+    tal2input.value = "";
 }
 
 function minus(){
-
+    const tal1= parseFloat(tal1input.value);
+    const tal2= parseFloat(tal2input.value);
+    let summa = tal1-tal2;
+    svar.value=summa;
+    let p = document.createElement("p");
+    p.textContent= tal1+"-"+tal2+"="+summa;
+    output.appendChild(p);
+    tal1input.value = "";
+    tal2input.value = "";
 }
 
 function ggr(){
-
+    const tal1= parseFloat(tal1input.value);
+    const tal2= parseFloat(tal2input.value);
+    let summa = tal1*tal2;
+    svar.value=summa;
+    let p = document.createElement("p");
+    p.textContent= tal1+"*"+tal2+"="+summa;
+    output.appendChild(p);
+    tal1input.value = "";
+    tal2input.value = "";
 }
 
 function dela(){
-
+    const tal1= parseFloat(tal1input.value);
+    const tal2= parseFloat(tal2input.value);
+    let summa = tal1/tal2;
+    svar.value=summa;
+    let p = document.createElement("p");
+    p.textContent= tal1+"/"+tal2+"="+summa;
+    output.appendChild(p);
+    tal1input.value = "";
+    tal2input.value = "";
 }
-
-let output = document.querySelector("output");
-output.appendChild(p);
-tal1input.value = "";
-tal2input.value = "";
-// output grejer
-
 console.log("Matte!");
+
+// parseFloat = decimaltal
